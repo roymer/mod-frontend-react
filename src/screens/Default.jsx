164f0260d7d02cc718f329df281b67e99df7276f
Roymer Ticona <roymerticoma@gmail.com>
2024-08-14 21:37:22 -0400

    fixed promp LessonInfo

--- src/screens/Default.jsx
+++ src/screens/Default.jsx
@@ -1,6 +1,7 @@
 import Logo from './_components/Logo';
 import Description from './_components/Description';
 import OpenLink from '../components/OpenLink';
+import LessonInfo from './_components/LessonInfo';
 
 const Default = () => {
     return (
@@ -10,6 +11,8 @@ const Default = () => {
                 <Description />
                 <OpenLink tittle="Aprende React" url="https://react.dev"/>
                 <OpenLink tittle="Mi Git aquí" url="https://github.com/roymer/mod-frontend-react"/>
+                <LessonInfo number="1" text="Introducción a react y estructura de proyecto" />
+                <LessonInfo number="2" text="Context API para la gestión del estado global en aplicaciones react" />
             </header>
         </div>
     );
---
+++ src/screens/_components/LessonInfo.jsx
@@ -0,0 +1,9 @@
+const LessonInfo = ({ number, text }) => {
+    return (
+        <div>
+            <p>{number}: {text}</p>
+        </div>
+    );
+};
+
+export default LessonInfo;
