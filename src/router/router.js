9ed3e3c521092a087334f72b8a3b011d2c254cec
Roymer Ticona <roymerticoma@gmail.com>
2024-08-22 19:05:28 -0400

    adding github pages

--- src/router/router.js
+++ src/router/router.js
@@ -6,7 +6,7 @@ import LoginForm from "../screens/Forms/LoginForm";
 
 import App from "../App";
 
-const basename = process.env.NODE_ENV === 'production' ? '/desarrollo-frontend-react' : '/';
+const basename = process.env.NODE_ENV === 'production' ? '/mod-frontend-react' : '/';
 
 const routes = createBrowserRouter([
     {
