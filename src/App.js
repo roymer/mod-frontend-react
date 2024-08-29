b8fdab0f01c7b3ffc1567cd0e5d1895d37f7ae9e
Roymer Ticona <roymerticoma@gmail.com>
2024-08-28 21:00:51 -0400

    fixed nav login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 
-//import Navbar from "./components/Navbar";
+import LoginForm from './screens/Forms/LoginForm';
+import Navbar from "./components/Navbar";
 import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
     <>
         {/* <Navbar /> */}
+        <LoginForm />
         <Outlet />
     </>
   );
